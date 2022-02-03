aa76b8ba9daf0ed12e2a06a613fc187d66c4a548
Shiori <50712428+Oraicon@users.noreply.github.com>
2022-02-03 13:20:05 +0700

    +tab&modal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'modal-kegiatan',
+    loadChildren: () => import('./modal/modal-kegiatan/modal-kegiatan.module').then( m => m.ModalKegiatanPageModule)
   }
 ];
 @NgModule({
---
+++ src/app/modal/modal-kegiatan/modal-kegiatan.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-kegiatan',
+  templateUrl: './modal-kegiatan.page.html',
+  styleUrls: ['./modal-kegiatan.page.scss'],
+})
+export class ModalKegiatanPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
