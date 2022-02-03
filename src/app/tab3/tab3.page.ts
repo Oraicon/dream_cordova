3f2f2ada6e9342ba49fa52a83d9edf437c71ca85
Shiori <50712428+Oraicon@users.noreply.github.com>
2022-02-03 13:54:06 +0700

    profiledone

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,161 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertCtrl: AlertController) {}
+
+  //alert data untuk diubah
+  ubahusername(){
+    this.alertCtrl.create({
+      header: 'Perubahan nama pengguna',
+      message: 'Silahkan untuk mengisi nama pengguna baru anda',
+      inputs: [
+        {
+          name: 'username',
+          placeholder: 'Nama Pengguna',
+          type: 'text'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Batal',
+        },
+        {
+          text: 'Simpan',
+        }
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
+
+  ubahpassword(){
+    this.alertCtrl.create({
+      header: 'Perubahan sandi pengguna',
+      message: 'Silahkan untuk mengisi sandi pengguna baru anda',
+      inputs: [
+        {
+          name: 'password',
+          placeholder: 'Sandi Pengguna',
+          type: 'password'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Batal',
+        },
+        {
+          text: 'Simpan',
+        }
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
+
+  ubahnik(){
+    this.alertCtrl.create({
+      header: 'Perubahan nik pengguna',
+      message: 'Silahkan untuk mengisi nik anda',
+      inputs: [
+        {
+          name: 'nik',
+          placeholder: 'Nomor Induk Penduduk',
+          type: 'number'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Batal',
+        },
+        {
+          text: 'Simpan',
+        }
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
+
+  ubahemail(){
+    this.alertCtrl.create({
+      header: 'Perubahan email',
+      message: 'Silahkan untuk mengisi email anda',
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'Email',
+          type: 'email'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Batal',
+        },
+        {
+          text: 'Simpan',
+        }
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
+
+  ubahtanggallahir(){
+    this.alertCtrl.create({
+      header: 'Perubahan tanggal lahir',
+      message: 'Silahkan untuk mengisi tanggal lahir anda',
+      inputs: [
+        {
+          name: 'tgllahir',
+          type: 'date'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Batal',
+        },
+        {
+          text: 'Simpan',
+        }
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
+
+  ubahkelamin(){
+    this.alertCtrl.create({
+      header: 'Ubah Jenis Kelamin',
+      message: 'Pilih Jenis Kelamin Anda',
+      inputs: [
+        {
+          type: 'radio',
+          label: 'Laki-laki',
+          value: 'l'
+        },
+        {
+          type: 'radio',
+          label: 'Perempuan',
+          value: 'p'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Batal',
+          handler: (data: any) => {
+            console.log('Canceled', data);
+          }
+        },
+        {
+          text: 'Simpan',
+          handler: (data: any) => {
+            console.log('Selected Information', data);
+          }
+        }
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
 
 }
