83b01fdde4dc463c3abea9cff08f650b0e0b4606
Shiori <50712428+Oraicon@users.noreply.github.com>
2022-02-09 17:15:41 +0700

    uguardstatislocal

--- src/app/services/guard-service.service.ts
+++ src/app/services/guard-service.service.ts
@@ -1,20 +1,39 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+import { Router} from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuardServiceService {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private strg: Storage) { 
+    this.init();
+  }
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
-    console.log(route);
-    let authInfo = {
-      authenticated: false,
-    };
+  init() {
+    this.strg.create();
+    this.strg.set('auth', false);
+  }
+
+  async canActivate(){
+    
+    let authInfo;
+    const dataauth = await this.strg.get('auth');
+    
+    if(dataauth == true){
+      authInfo= {
+        authenticated: true
+      };
+    }else{
+      authInfo= {
+        authenticated: false
+      };
+    }
+
+    console.log(authInfo.authenticated);
     if (!authInfo.authenticated) {
-      this.router.navigate(['login']);
+      this.router.navigate(["/login"]);
       return false;
     }
     return true;
